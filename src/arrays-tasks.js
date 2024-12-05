5a6f554745d24470bb87aea8c0ef7d346cff1557
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2024-12-05 13:43:49 +0100

    feat: solve isSameLength

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -147,8 +147,8 @@ function getAverage(arr) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  return arr.every((innerItem) => innerItem.length === arr[0].length);
 }
 
 /**
