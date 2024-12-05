b7387aa7dd47524ba4796bf2b129f39db1c18dac
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2024-12-05 15:21:48 +0100

    feat: solve generateOdds

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -361,8 +361,11 @@ function createChunks(arr, chunkSize) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  if (len > 0) {
+    return Array.from({ length: len }, (_, index) => index * 2 + 1);
+  }
+  return [0];
 }
 
 /**
