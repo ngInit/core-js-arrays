5152bcab3584549b4ae189d9de4a1b76b7d08f00
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2024-12-05 16:42:42 +0100

    feat: solve getIdentityMatrix

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -418,8 +418,12 @@ function getFalsyValuesCount(arr) {
  *                              [0,0,0,1,0],
  *                              [0,0,0,0,1]]
  */
-function getIdentityMatrix(/* n */) {
-  throw new Error('Not implemented');
+function getIdentityMatrix(n) {
+  return Array.from({ length: n }, (_, a) =>
+    Array.from({ length: n }, (__, b) => {
+      return a === b ? 1 : 0;
+    })
+  );
 }
 
 /**
