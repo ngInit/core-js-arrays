9a71d317e7482a2de7f7f1bfb3fe2f8809260fb7
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2024-12-05 17:51:58 +0100

    feat: solve findCommonElements

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -491,8 +491,10 @@ function getMaxItems(arr, n) {
  *    findCommonElements(['a', 'b', 'c'], ['b', 'c', 'd']) => [ 'b', 'c' ]
  *    findCommonElements([1, 2, 3], ['a', 'b', 'c']) => []
  */
-function findCommonElements(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function findCommonElements(arr1, arr2) {
+  const arrayBig = arr1.length >= arr2.length ? arr1 : arr2;
+  const arraySmall = arr1.length >= arr2.length ? arr2 : arr1;
+  return arraySmall.filter((number) => arrayBig.includes(number));
 }
 
 /**
