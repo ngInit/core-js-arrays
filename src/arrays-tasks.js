3b2dca6aff77f69d21c361363d9fe3f9788dad08
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2024-12-05 15:09:24 +0100

    feat: solve createChunks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -342,8 +342,11 @@ function calculateBalance(arr) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize) {
+  const size = Math.ceil(arr.length / chunkSize);
+  return Array.from({ length: size }, (_, index) =>
+    arr.slice(index * chunkSize, index * chunkSize + chunkSize)
+  );
 }
 
 /**
